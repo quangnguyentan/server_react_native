8b528461a6400b0529cb6db2e5319a3503e24c01
unknown <tanquanga6k10@gmail.com>
2024-03-14 14:29:41 +0700

    push code

--- models/employees.js
+++ models/employees.js
@@ -9,16 +9,20 @@ var employeesSchema = new mongoose.Schema(
     },
     position: {
       type: String,
+      required: true,
     },
     department: {
       type: String,
+      required: true,
     },
     email: {
       type: String,
+      required: true,
     },
     phone: {
       type: String,
       required: true,
+      unique: true,
     },
   },
   {
